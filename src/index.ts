720ee7c55c43b587af7194c64d0b627d9ea940d6
Shota Mine <v25y0006@oita-u.ac.jp>
2025-05-26 07:13:21 +0000

    honoでOpenAPIの確認

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+import { zValidator } from '@hono/zod-validator'
+import { createRoute, OpenAPIHono } from '@hono/zod-openapi'
+import { swaggerUI } from '@hono/swagger-ui'
 
 const app = new Hono()
+
+app.use(logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/api', (c) => {
-  const query = c.req.query('name')
-  return c.json({ message: `Hello ${query}!` })
-})
+app.post('/api',
+  zValidator('form', z.object({ body: z.string() })),
+  (c) => {
+    const validated = c.req.valid('form')
+    return c.text(`POST /api`)
+  }
+)
+
+app.notFound((c) => c.text('Custom 404 Message', 404) )
 
 export default app
