cbc9f68c85cf8fdca22e82b7f8d59e2f934cfc47
Shota Mine <v25y0006@oita-u.ac.jp>
2025-06-09 07:23:01 +0000

    WebAPIの利用と設計

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,23 @@ app.get('/ui',
   })
 )
 
+app.get('/github-models', async (c) => {
+  const apiUrl = 'https://models.github.ai/inference/chat/completions'
+  const headers = {
+    'Authorization': `Bearer ${c.env.GITHUB_TOKEN}`,
+    'Content-Type': 'application/json'
+  }
+
+  const body = JSON.stringify({
+    messages: [{ role: 'user', content: '生成AIを用いたコーディング手法とは？' }],
+    model: 'openai/gpt-4.1'
+  })
+
+  const response = await fetch(apiUrl, { method: 'POST', headers, body })
+  const data = await response.json()
+  return c.text(data.choices[0].message.content)
+})
+
 app.notFound((c) => c.text('Custom 404 Message', 404) )
 
 export default app
